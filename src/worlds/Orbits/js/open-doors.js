b604c707110c44b8e0be7758b0b776477dc5711b
hujillian <hujillian123@gmail.com>
2023-03-16 20:28:33 -0400

    open doors on button press

---
+++ src/worlds/Orbits/js/open-doors.js
@@ -0,0 +1,82 @@
+let isOpen_lobbyFront = false;
+let isOpen_diningFront = false;
+
+const moveDoors = function(door, doorPos, changeInY){
+
+    door.setAttribute('animation', "property: position; to: "+ doorPos.x + " " + (doorPos.y + changeInY) + " " + doorPos.z + "; dur: 500; easing: easeInQuad;");
+
+}
+
+AFRAME.registerComponent('open-doors', {
+    schema: {},
+
+    init() {
+
+        const CONTEXT_AF = this; 
+
+        CONTEXT_AF.leftDoor = document.querySelector('#lobbydoor_L_front');
+        CONTEXT_AF.leftDoor.pos = CONTEXT_AF.leftDoor.getAttribute('position');
+
+        CONTEXT_AF.rightDoor = document.querySelector('#lobbydoor_R_front');
+        CONTEXT_AF.rightDoor.pos = CONTEXT_AF.rightDoor.getAttribute('position');
+        
+        // when you click the button, it opens or closes the doors
+        CONTEXT_AF.el.addEventListener('click', function(){
+            if(isOpen_lobbyFront){
+                // close doors
+                // CONTEXT_AF.leftDoor.setAttribute('animation', "property: position; to: "+ (CONTEXT_AF.leftDoor.pos.x + 3) + " " + CONTEXT_AF.leftDoor.pos.y + " " + CONTEXT_AF.leftDoor.pos.z + "; dur: 500; easing: easeInQuad;");
+                // CONTEXT_AF.rightDoor.setAttribute('animation', "property: position; to: "+ (CONTEXT_AF.rightDoor.pos.x - 3) + " " + CONTEXT_AF.leftDoor.pos.y + " " + CONTEXT_AF.leftDoor.pos.z + "; dur: 500; easing: easeInQuad;");
+                moveDoors(CONTEXT_AF.leftDoor, CONTEXT_AF.leftDoor.pos, -6);
+                moveDoors(CONTEXT_AF.rightDoor, CONTEXT_AF.rightDoor.pos, -6);
+                isOpen_lobbyFront = false;
+            }
+            else{
+                // open door
+                // CONTEXT_AF.leftDoor.setAttribute('animation', "property: position; to: "+ (CONTEXT_AF.leftDoor.pos.x - 3) + " " + CONTEXT_AF.leftDoor.pos.y + " " + CONTEXT_AF.leftDoor.pos.z + "; dur: 500; easing: easeInQuad;");
+                // CONTEXT_AF.rightDoor.setAttribute('animation', "property: position; to: "+ (CONTEXT_AF.rightDoor.pos.x + 3) + " " + CONTEXT_AF.leftDoor.pos.y + " " + CONTEXT_AF.leftDoor.pos.z + "; dur: 500; easing: easeInQuad;");
+                moveDoors(CONTEXT_AF.leftDoor, CONTEXT_AF.leftDoor.pos, 6);
+                moveDoors(CONTEXT_AF.rightDoor, CONTEXT_AF.rightDoor.pos, 6);
+                isOpen_lobbyFront = true;
+            }
+        });
+
+    },
+
+});
+
+AFRAME.registerComponent('open-doors-2', {
+    schema: {},
+
+    init() {
+
+        const CONTEXT_AF = this; 
+
+        CONTEXT_AF.leftDoor = document.querySelector('#diningHall_L_front');
+        CONTEXT_AF.leftDoor.pos = CONTEXT_AF.leftDoor.getAttribute('position');
+
+        CONTEXT_AF.rightDoor = document.querySelector('#diningHall_R_front');
+        CONTEXT_AF.rightDoor.pos = CONTEXT_AF.rightDoor.getAttribute('position');
+        
+        // when you click the button, it opens or closes the doors
+        CONTEXT_AF.el.addEventListener('click', function(){
+            if(isOpen_diningFront){
+                // close doors
+                // CONTEXT_AF.leftDoor.setAttribute('animation', "property: position; to: "+ (CONTEXT_AF.leftDoor.pos.x + 3) + " " + CONTEXT_AF.leftDoor.pos.y + " " + CONTEXT_AF.leftDoor.pos.z + "; dur: 500; easing: easeInQuad;");
+                // CONTEXT_AF.rightDoor.setAttribute('animation', "property: position; to: "+ (CONTEXT_AF.rightDoor.pos.x - 2.6) + " " + CONTEXT_AF.leftDoor.pos.y + " " + CONTEXT_AF.leftDoor.pos.z + "; dur: 500; easing: easeInQuad;");
+                moveDoors(CONTEXT_AF.leftDoor, CONTEXT_AF.leftDoor.pos, -6);
+                moveDoors(CONTEXT_AF.rightDoor, CONTEXT_AF.rightDoor.pos, -6);
+                isOpen_diningFront = false;
+            }
+            else{
+                // open door
+                // CONTEXT_AF.leftDoor.setAttribute('animation', "property: position; to: "+ (CONTEXT_AF.leftDoor.pos.x - 3) + " " + CONTEXT_AF.leftDoor.pos.y + " " + CONTEXT_AF.leftDoor.pos.z + "; dur: 500; easing: easeInQuad;");
+                // CONTEXT_AF.rightDoor.setAttribute('animation', "property: position; to: "+ (CONTEXT_AF.rightDoor.pos.x + 2.6) + " " + CONTEXT_AF.leftDoor.pos.y + " " + CONTEXT_AF.leftDoor.pos.z + "; dur: 500; easing: easeInQuad;");
+                moveDoors(CONTEXT_AF.leftDoor, CONTEXT_AF.leftDoor.pos, 6);
+                moveDoors(CONTEXT_AF.rightDoor, CONTEXT_AF.rightDoor.pos, 6);
+                isOpen_diningFront = true;
+            }
+        });
+
+    },
+
+});
